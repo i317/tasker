32352d46f79e7e800c3cd824a903248da2940b0a
bp70291 <bp70291@umbc.edu>
2017-03-24 20:21:50 -0400

    Add test database

---
+++ TaskerDB.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE tasker (
+taskID integer,
+question char(200),
+tweet char(200),
+userAnswer char(200),
+correctAnswer char(200)
+
+);
+COPY tasker FROM '/Users/Imauri/Documents/Tasker/Task Database - Self Driving Car Tweet Sentiment.csv' DELIMITER ','CSV HEADER
+
+SELECT * 
+FROM tasker;
