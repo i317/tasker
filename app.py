3ee750e78cb89765383877068bcdf3779654b713
bp70291 <bp70291@umbc.edu>
2017-03-27 22:24:06 -0400

    Update of interactive.html file
    HTML w/sample question
    Update of backend rendering HTML and getting response from textbox

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return 'OK!'
 
-if __name__== "__main__":
-	app.run()
+@app.route('/interactive/')
+def interactive():
+	return render_template('interactive.html')
 
+@app.route('/interactive/', methods=['POST'])
+def result_post():
+    response = request.form['proglang']
+    return response
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
